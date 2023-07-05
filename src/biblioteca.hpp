3fe6af5fa53f47706744e3635c2e18b6d0dea37b
João Bicalho <joaobicalho03@gmail.com>
2023-07-04 23:38:54 -0300

    updates

--- src/biblioteca.hpp
+++ src/biblioteca.hpp
@@ -11,8 +11,6 @@
 #include <chrono>
 //#include "labirinto.hpp"
 
-
-
 class Player {
 public:
 	Player(float x, float y, const Maze &maze) :
@@ -75,10 +73,9 @@ public:
 	}
 
 	void setPosition(float x, float y) {
-	        m_x = x;
-	        m_y = y;
-	    }
-
+		m_x = x;
+		m_y = y;
+	}
 
 private:
 	unsigned int m_x;
@@ -122,7 +119,6 @@ public:
 		if (newX >= 0 && newX < m_maze.GetWidth() && newY >= 0
 				&& newY < m_maze.GetHeight()) {
 
-
 			// Verificar se a célula na nova posição é vazia (false)
 			if (m_maze.GetCellValue(newX, newY) && !(newX == 2 && newY == 0)
 					&& !(newX == 10 * tamLab && newY == 12 * tamLab)) {
@@ -156,17 +152,17 @@ public:
 	}
 
 	float GetX() const {
-			return m_x;
-		}
+		return m_x;
+	}
 
-		float GetY() const {
-			return m_y;
-		}
+	float GetY() const {
+		return m_y;
+	}
 
-		void setPosition(float x, float y) {
-		        m_x = x;
-		        m_y = y;
-		    }
+	void setPosition(float x, float y) {
+		m_x = x;
+		m_y = y;
+	}
 
 private:
 	unsigned int m_x;
@@ -174,155 +170,224 @@ private:
 	const Maze &m_maze;
 };
 
-void MovePlayer(const sf::Event& event, Player& player, bool& isMoving) {
-    if (event.type == sf::Event::KeyPressed) {
-        if (event.key.code == sf::Keyboard::Up) {
-            player.Move(0, -1);
-        } else if (event.key.code == sf::Keyboard::Down) {
-            player.Move(0, 1);
-        } else if (event.key.code == sf::Keyboard::Left) {
-            player.Move(-1, 0);
-        } else if (event.key.code == sf::Keyboard::Right) {
-            player.Move(1, 0);
-        }
-       // sf::sleep(sf::milliseconds(1));
-    } else if (event.type == sf::Event::KeyReleased) {
+void MovePlayer(const sf::Event &event, Player &player, bool &isMoving) {
+	if (event.type == sf::Event::KeyPressed) {
+		if (event.key.code == sf::Keyboard::Up) {
+			player.Move(0, -1);
+		} else if (event.key.code == sf::Keyboard::Down) {
+			player.Move(0, 1);
+		} else if (event.key.code == sf::Keyboard::Left) {
+			player.Move(-1, 0);
+		} else if (event.key.code == sf::Keyboard::Right) {
+			player.Move(1, 0);
+		}
+		// sf::sleep(sf::milliseconds(1));
+	} else if (event.type == sf::Event::KeyReleased) {
 
-        if (event.key.code == sf::Keyboard::Up || event.key.code == sf::Keyboard::Down ||
-            event.key.code == sf::Keyboard::Left || event.key.code == sf::Keyboard::Right) {
-            isMoving = false;
+		if (event.key.code == sf::Keyboard::Up
+				|| event.key.code == sf::Keyboard::Down
+				|| event.key.code == sf::Keyboard::Left
+				|| event.key.code == sf::Keyboard::Right) {
+			isMoving = false;
 
-        }
-    }
+		}
+	}
 }
 
-void MovePlayer2(const sf::Event& event, Player& player2, bool& isMoving) {
-    if (event.type == sf::Event::KeyPressed) {
-        if (event.key.code == sf::Keyboard::W) {
-            player2.Move(0, -1);
-        } else if (event.key.code == sf::Keyboard::S) {
-            player2.Move(0, 1);
-        } else if (event.key.code == sf::Keyboard::A) {
-            player2.Move(-1, 0);
-        } else if (event.key.code == sf::Keyboard::D) {
-            player2.Move(1, 0);
-        }
+void MovePlayer2(const sf::Event &event, Player &player2, bool &isMoving) {
+	if (event.type == sf::Event::KeyPressed) {
+		if (event.key.code == sf::Keyboard::W) {
+			player2.Move(0, -1);
+		} else if (event.key.code == sf::Keyboard::S) {
+			player2.Move(0, 1);
+		} else if (event.key.code == sf::Keyboard::A) {
+			player2.Move(-1, 0);
+		} else if (event.key.code == sf::Keyboard::D) {
+			player2.Move(1, 0);
+		}
 
-      //  sf::sleep(sf::milliseconds(1));
-    } else if (event.type == sf::Event::KeyReleased) {
-        if (event.key.code == sf::Keyboard::W || event.key.code == sf::Keyboard::S ||
-            event.key.code == sf::Keyboard::A || event.key.code == sf::Keyboard::D) {
-            isMoving = false;
-        }
-    }
+		//  sf::sleep(sf::milliseconds(1));
+	} else if (event.type == sf::Event::KeyReleased) {
+		if (event.key.code == sf::Keyboard::W
+				|| event.key.code == sf::Keyboard::S
+				|| event.key.code == sf::Keyboard::A
+				|| event.key.code == sf::Keyboard::D) {
+			isMoving = false;
+		}
+	}
 }
 
-
-	bool CheckCollision(const Player &player, const Inimigo & inimigo){
+bool CheckCollision(const Player &player, const Inimigo &inimigo) {
 	float playerX = player.GetX();
 	float playerY = player.GetY();
 	float inimigoX = inimigo.GetX();
 	float inimigoY = inimigo.GetY();
 
-		if(playerX == inimigoX && playerY == inimigoY){
-			return true;
-		}
+	if (playerX == inimigoX && playerY == inimigoY) {
+		return true;
+	}
+	return false;
+}
+
+bool playerNaSaida(const Player &player, const Player &player2,
+		unsigned int saidaX, unsigned int saidaY) {
+	if (player.GetX() == saidaX && player.GetY() == saidaY
+			|| player2.GetX() == saidaX && player2.GetY() == saidaY) {
+		return true;
+	} else {
 		return false;
 	}
 
-	bool playerNaSaida(const Player &player,const Player &player2,unsigned int saidaX, unsigned int saidaY){
-		if (player.GetX() == saidaX && player.GetY() == saidaY || player2.GetX() == saidaX && player2.GetY() == saidaY ) {
-		    return true;
+}
+
+void labirinto() {
+	std::srand(static_cast<unsigned int>(std::time(nullptr)));
+	int tamLab = 5;
+	bool isEnemyVisible = false;
+
+	sf::RenderWindow window(sf::VideoMode(960, 800),
+			"Sponge Bob SquarePants MazeCraze");
+	sf::Texture playerTexture;
+	if (!playerTexture.loadFromFile("assets/bobEsponja.png")) {
+		std::cout << "nao abre" << std::endl;
+		return;
+	}
+	sf::Texture playerTexture2;
+	if (!playerTexture2.loadFromFile("assets/patrick.png")) {
+		std::cout << "nao abre" << std::endl;
+		return;
+	}
+	sf::Texture enemyTexture;
+	if (!enemyTexture.loadFromFile("assets/enemy1.png")) {
+		std::cout << "nao abre" << std::endl;
+		return;
+	}
+	sf::Texture background;
+	if (!background.loadFromFile("assets/background2.png")) {
+		std::cout << "nao abre" << std::endl;
+		return;
+	}
+	sf::Sprite backgroundSprite(background);
+
+	while (window.isOpen()) {
+		Maze m(39 / tamLab, 39 / tamLab);
+		m.Generate();
+		Player player(2, 1, m);
+		Player player2(2, 1, m);
+		bool isMoving = false;
+		if (tamLab <= 3) {
+			Inimigo enemy(20 / tamLab, 20 / tamLab, m);
+			// Resto do código do loop principal
+
+			float cellSize = std::min(
+					static_cast<float>(window.getSize().x) / 800,
+					static_cast<float>(window.getSize().y) / 800);
+
+			while (true) {
+				sf::Event event;
+				while (window.pollEvent(event)) {
+					if (event.type == sf::Event::Closed) {
+						window.close();
+						return;
+					}
+					MovePlayer(event, player, isMoving);
+					MovePlayer2(event, player2, isMoving);
+				}
+
+				if (tamLab <= 3) {
+					isEnemyVisible = true;
+				} else {
+					isEnemyVisible = false;
+				}
+
+				if (CheckCollision(player, enemy)
+						|| CheckCollision(player2, enemy)) {
+					std::cout << "bateu" << std::endl;
+				}
+
+				unsigned int saidaX = m.GetWidth() - 3;
+				unsigned int saidaY = m.GetHeight() - 2;
+
+				if (playerNaSaida(player, player2, saidaX, saidaY)) {
+					tamLab--;
+					break;
+				}
+
+				backgroundSprite.setPosition(0, 0);
+				backgroundSprite.setScale(
+						static_cast<float>(window.getSize().x)
+								/ background.getSize().x,
+						static_cast<float>(window.getSize().y)
+								/ background.getSize().y);
+
+				window.clear();
+				window.draw(backgroundSprite);
+				m.desenharQuadrados(window, m);
+
+				float cellSize = std::min(
+						static_cast<float>(window.getSize().x) / m.GetWidth(),
+						static_cast<float>(window.getSize().y) / m.GetHeight());
+
+				player.Draw(window, m, cellSize, playerTexture);
+				player2.Draw(window, m, cellSize, playerTexture2);
+				if (isEnemyVisible) {
+					enemy.MoveRandomly(tamLab);
+					enemy.Draw(window, m, cellSize, enemyTexture);
+				}
+
+				window.display();
+			}
 		} else {
-		   return false;
+			float cellSize = std::min(
+					static_cast<float>(window.getSize().x) / 800,
+					static_cast<float>(window.getSize().y) / 800);
+
+			while (true) {
+				sf::Event event;
+				while (window.pollEvent(event)) {
+					if (event.type == sf::Event::Closed) {
+						window.close();
+						return;
+					}
+					MovePlayer(event, player, isMoving);
+					MovePlayer2(event, player2, isMoving);
+				}
+
+				if (tamLab <= 3) {
+					isEnemyVisible = true;
+				} else {
+					isEnemyVisible = false;
+				}
+
+				unsigned int saidaX = m.GetWidth() - 3;
+				unsigned int saidaY = m.GetHeight() - 2;
+
+				if (playerNaSaida(player, player2, saidaX, saidaY)) {
+					tamLab--;
+					break;
+				}
+
+				backgroundSprite.setPosition(0, 0);
+				backgroundSprite.setScale(
+						static_cast<float>(window.getSize().x)
+								/ background.getSize().x,
+						static_cast<float>(window.getSize().y)
+								/ background.getSize().y);
+
+				window.clear();
+				window.draw(backgroundSprite);
+				m.desenharQuadrados(window, m);
+
+				float cellSize = std::min(
+						static_cast<float>(window.getSize().x) / m.GetWidth(),
+						static_cast<float>(window.getSize().y) / m.GetHeight());
+
+				player.Draw(window, m, cellSize, playerTexture);
+				player2.Draw(window, m, cellSize, playerTexture2);
+
+				window.display();
+			}
 		}
-
 	}
-
-
-	void labirinto() {
-	    std::srand(static_cast<unsigned int>(std::time(nullptr)));
-	    int tamLab = 5;
-
-	    sf::RenderWindow window(sf::VideoMode(960, 800), "Sponge Bob SquarePants MazeCraze");
-	    sf::Texture playerTexture;
-	    if (!playerTexture.loadFromFile("assets/bobEsponja.png")) {
-	        std::cout << "nao abre" << std::endl;
-	        return;
-	    }
-	    sf::Texture playerTexture2;
-	   	if (!playerTexture2.loadFromFile("assets/patrick.png")) {
-	   	    std::cout << "nao abre" << std::endl;
-	   	    return;
-	   	    }
-	    sf::Texture enemyTexture;
-	    if (!enemyTexture.loadFromFile("assets/enemy1.png")) {
-	        std::cout << "nao abre" << std::endl;
-	        return;
-	    }
-	    sf::Texture background;
-	    if (!background.loadFromFile("assets/background2.png")) {
-	        std::cout << "nao abre" << std::endl;
-	        return;
-	    }
-	    sf::Sprite backgroundSprite(background);
-
-	    while (window.isOpen()) {
-	        Maze m(39 / tamLab, 39 / tamLab);
-	        m.Generate();
-	        Player player(2, 1, m);
-	        Player player2(2, 1, m);
-	        Inimigo enemy(20 / tamLab, 20 / tamLab, m);
-	        bool isMoving = false;
-
-	        float cellSize = std::min(static_cast<float>(window.getSize().x) / 800,
-	            static_cast<float>(window.getSize().y) / 800);
-
-	        while (true) {
-	            sf::Event event;
-	            while (window.pollEvent(event)) {
-	                if (event.type == sf::Event::Closed) {
-	                    window.close();
-	                    return;
-	                }
-	                MovePlayer(event, player, isMoving);
-	                MovePlayer2(event, player2, isMoving);
-	            }
-
-	            enemy.MoveRandomly(tamLab);
-
-	            if (CheckCollision(player, enemy) || CheckCollision(player2, enemy)) {
-	                std::cout << "bateu" << std::endl;
-	            }
-
-	            unsigned int saidaX = m.GetWidth() - 3;
-	            unsigned int saidaY = m.GetHeight() - 2;
-
-	            if (playerNaSaida(player,player2 ,saidaX, saidaY)) {
-	                tamLab--;
-	                break;
-	            }
-
-	            backgroundSprite.setPosition(0, 0);
-	            backgroundSprite.setScale(
-	                static_cast<float>(window.getSize().x) / background.getSize().x,
-	                static_cast<float>(window.getSize().y) / background.getSize().y);
-
-	            window.clear();
-	            window.draw(backgroundSprite);
-	            m.desenharQuadrados(window, m);
-
-	            float cellSize = std::min(
-	                static_cast<float>(window.getSize().x) / m.GetWidth(),
-	                static_cast<float>(window.getSize().y) / m.GetHeight());
-
-	            player.Draw(window, m, cellSize, playerTexture);
-	            player2.Draw(window, m, cellSize, playerTexture2);
-	            enemy.Draw(window, m, cellSize, enemyTexture);
-
-	            window.display();
-	        }
-	    }
-	}
-
+}
 
